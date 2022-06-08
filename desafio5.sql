7280c83ea55782e7a187854c4df136dc8f1449cc
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-06-08 15:04:21 -0300

    05: More listeneds songs

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT songs.name AS cancao, COUNT(music_history.song_id) AS reproducoes
+FROM SpotifyClone.songs
+INNER JOIN SpotifyClone.music_history
+ON songs.song_id=music_history.song_id
+GROUP BY music_history.song_id
+ORDER BY COUNT(music_history.song_id) DESC
+LIMIT 2
